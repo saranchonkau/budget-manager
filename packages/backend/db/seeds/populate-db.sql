2ab1727bafcfc6716cdea58c330ef6d27a8e6d04
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2021-12-09 15:18:20 +0300

    Update seeds

--- packages/backend/db/seeds/populate-db.sql
+++ packages/backend/db/seeds/populate-db.sql
@@ -1,3 +1,5 @@
-INSERT INTO "user" (id, email, password_hash, name, created_at, updated_at, deleted_at)
-VALUES ('438ea40a-53b7-4781-9bfd-e2ed3fae3b4d', 'ivan.saranchonkau@test.com', '$2b$10$cLBU6uqdgPABPIMW7CJYvOjB2Z5q5TqrMVHd8yrLvu.Z7gh1UFJES',
-        'Ivan Saranchonkau', 'now', 'now', 'epoch');
+INSERT INTO public."user" (id, email, password_hash, name, created_at, updated_at, deleted_at)
+VALUES ('72b35fbe-4a9b-495e-81a5-7bce62aeea05', 'saranchonkau@gmail.com',
+        'ae5b93416098e2d3:091db7e52ac0440383363f0158ea1172d42b0405658954c7df69177805cb257c10e5df1536be167fde719e95866e13964029aef58a6cf4ab651aab3f6422079a',
+        'Ivan', '2021-12-08 17:51:43.373000 +00:00', '2021-12-08 17:51:43.373000 +00:00',
+        '1970-01-01 00:00:00.000000 +00:00');
