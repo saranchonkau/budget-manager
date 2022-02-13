3790e47437a5074d396272cba32400afc6ab1165
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2022-02-14 00:03:33 +0300

    add docker-compose config

--- build-backend.sh
+++ build-backend.sh
@@ -3,9 +3,4 @@
 docker build \
   --tag budget-manager-backend-image \
   --file ./packages/backend/docker/Dockerfile \
-  --build-arg DATABASE_USER=postgres \
-  --build-arg DATABASE_PASSWORD=1234567 \
-  --build-arg DATABASE_NAME=budget-manager \
-  --build-arg JWT_SIGN_SECRET=myrS@@e7fTZdIT6$u^$POGkIOzchQ3 \
-  --build-arg PORT=9000 \
   .
